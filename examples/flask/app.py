330d6ab95fa786cb6b3491de334d80ee97b30c3c
Aidan Feldman <aidan.feldman@gmail.com>
2025-02-03 23:40:15 -0500

    have Flask example show a DataFrame

--- examples/flask/app.py
+++ examples/flask/app.py
@@ -1,10 +1,19 @@
-"""https://flask.palletsprojects.com/en/stable/quickstart/"""
+"""Based on https://stackoverflow.com/a/52644615/358804"""
 
-from flask import Flask
+from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
+df = pd.DataFrame(
+    {
+        "A": [0, 1, 2, 3, 4],
+        "B": [5, 6, 7, 8, 9],
+        "C": ["a", "b", "c--", "d", "e"],
+    }
+)
+
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def show_table():
+    return render_template("index.html", table=df.to_html())
--- readings/week_04.md
+++ readings/week_04.md
@@ -2,6 +2,8 @@
 
 [General information](../README.md#readings)
 
+- [50 Things That Made the Modern Economy: Interface Message Processor](https://www.bbc.co.uk/programmes/w3csz2wx)
+- [Getting started with the web](https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/Dealing_with_files), Dealing With Files through CSS Basics
 - [Streamlit Getting Started](https://docs.streamlit.io/get-started)
 
 Don't forget to [submit a response](../README.md#responses).
