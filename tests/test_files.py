a6e2d29995cf7d8b459eefff6c16d76dcb72f62e
Aidan Feldman <aidan.feldman@gmail.com>
2025-04-10 17:29:48 -0400

    no submission for lab 12

--- tests/test_files.py
+++ tests/test_files.py
@@ -5,6 +5,8 @@ import pytest
 readings = glob("readings/*")
 labs = glob("labs/*")
 
+LABS_WITHOUT_SUBMISSION = [1, 12]
+
 
 def read_file(path):
     with open(path) as f:
@@ -22,7 +24,7 @@ def test_readings_boilerplate(file):
 def test_lab_submit_info(file):
     if "example" in file:
         pytest.skip("Skip examples")
-    for skip_lab in [1]:
+    for skip_lab in LABS_WITHOUT_SUBMISSION:
         if file.endswith(f"/lab_{skip_lab:02}.md"):
             pytest.skip(f"Lab {skip_lab} doesn't have a separate submission")
 
